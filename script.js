f3c03c14ffdccda2bf6d5278ad8a93a0a8c6872a
Sasa <s.skoric@hotmail.com>
2023-04-30 19:09:07 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+
+const dropdowns = document.querySelectorAll(".dropdown")
+
+dropdowns.forEach(dropdown => {
+  const select = dropdown.querySelector(".select")
+  const arrow = dropdown.querySelector(".arrow")
+  const menu = dropdown.querySelector(".menu")
+  const options = dropdown.querySelectorAll('.menu li')
+  const selected = dropdown.querySelector('.select-ed')
+
+  select.addEventListener('click' , () =>{
+    select.classList.toggle('select-clicked')
+
+    arrow.classList.toggle('arrow-rotate')
+
+    menu.classList.toggle('menu-open')
+  })
+
+  options.forEach(option => {
+    option.addEventListener('click' , () =>{
+      selected.innerText = option.innerText
+
+      select.classList.remove('select-clicked')
+
+      arrow.classList.remove('arrow-rotate')
+
+      menu.classList.remove('menu-open')
+
+      options.forEach(option => {
+        option.classList.remove('active')
+      })
+
+      option.classList.add('active')
+    })
+  })
+})
